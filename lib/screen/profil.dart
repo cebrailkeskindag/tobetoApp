285b8dfa64b52d8238ee92ff6d52d77246fa3b90
Ilker Muhsin Şahin <ilkermsahin42@gmail.com>
2023-12-14 20:50:38 +0300

    sayfa eklendi

---
+++ lib/screen/profil.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Profil extends StatefulWidget {
+  const Profil({ Key? key }) : super(key: key);
+
+  @override
+  _ProfilState createState() => _ProfilState();
+}
+
+class _ProfilState extends State<Profil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
