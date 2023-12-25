4d72ad53de2c4436381fe53aaf30bcc94b259d07
Ilker Muhsin Şahin <ilkermsahin42@gmail.com>
2023-12-25 23:58:34 +0300

    TabBar menu eklendi

--- lib/screen/profil.dart
+++ lib/screen/profil.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:tobetoapp/widgets/circular_button.dart';
 import 'package:tobetoapp/widgets/drawer.dart';
 
+const List<Tab> tabs = <Tab>[
+  Tab(text: 'Başvurularım'),
+  Tab(text: 'Eğitimlerim'),
+  Tab(text: 'Duyuru ve Haberlerim'),
+  Tab(text: 'Anketlerim'),
+];
+
 class Profil extends StatefulWidget {
   const Profil({Key? key}) : super(key: key);
 
@@ -13,13 +20,38 @@ class Profil extends StatefulWidget {
 class _ProfilState extends State<Profil> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const DrawerMenu(),
-      appBar: AppBar(
-        title: const Text("Profil"),
-      ),
-      body: Scaffold(),
-      floatingActionButton: CircularButton(),
+    return DefaultTabController(
+      length: tabs.length,
+      // The Builder widget is used to have a different BuildContext to access
+      // closest DefaultTabController.
+      child: Builder(builder: (BuildContext context) {
+        final TabController tabController = DefaultTabController.of(context);
+        tabController.addListener(() {
+          if (!tabController.indexIsChanging) {
+            // Your code goes here.
+            // To get index of current tab use tabController.index
+          }
+        });
+        return Scaffold(
+          drawer: const DrawerMenu(),
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: tabs,
+            ),
+          ),
+          floatingActionButton: CircularButton(),
+          body: TabBarView(
+            children: tabs.map((Tab tab) {
+              return Center(
+                child: Text(
+                  '${tab.text!} Tab',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              );
+            }).toList(),
+          ),
+        );
+      }),
     );
   }
 }
