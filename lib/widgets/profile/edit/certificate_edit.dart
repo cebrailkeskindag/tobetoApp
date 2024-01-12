c08d71bfb5915a269c0b40f386c557641c7e9ea4
cebrailkeskindag <cebrailkeskindag1549@gmail.com>
2024-01-12 23:01:20 +0300

    profile edit tabbar eklendi

--- lib/widgets/profile/edit/certificate_edit.dart
+++ lib/widgets/profile/edit/certificate_edit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CertificateEdit extends StatefulWidget {
-  const CertificateEdit({ Key? key }) : super(key: key);
+  const CertificateEdit({Key? key}) : super(key: key);
 
   @override
   _CertificateEditState createState() => _CertificateEditState();
@@ -11,7 +11,34 @@ class _CertificateEditState extends State<CertificateEdit> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Sertifikalarım",
+              style: TextStyle(color: Colors.deepPurple, fontSize: 26),
+            ),
+            const SizedBox(height: 15),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                color: Colors.grey.shade100,
+                height: 200,
+                width: 350,
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.file_upload_outlined),
+                    Text("Dosya Yükle")
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/profile/edit/social_media_edit.dart
+++ lib/widgets/profile/edit/social_media_edit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SocialMediaEdit extends StatefulWidget {
-  const SocialMediaEdit({ Key? key }) : super(key: key);
+  const SocialMediaEdit({Key? key}) : super(key: key);
 
   @override
   _SocialMediaEditState createState() => _SocialMediaEditState();
@@ -11,7 +11,37 @@ class _SocialMediaEditState extends State<SocialMediaEdit> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: double.infinity,
+      height: 500,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Seçiniz*",
+                    hintStyle: const TextStyle(fontFamily: "Poppins"),
+                    suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.keyboard_arrow_down)),
+                    border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)))),
+              ),
+              const SizedBox(height: 20),
+              const TextField(
+                decoration: InputDecoration(
+                    hintText: "https://",
+                    hintStyle: TextStyle(fontFamily: "Poppins"),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)))),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(onPressed: () {}, child: const Text("Kaydet"))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
